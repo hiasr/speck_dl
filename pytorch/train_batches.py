da2869bd13f4c5a01a36653400c8ee2eb993d29d
Ruben Hias <ruben.hias@gmail.com>
2021-08-23 16:44:13 +0200

    Added both single and batched validation phase

--- pytorch/train_batches.py
+++ pytorch/train_batches.py
@@ -12,12 +12,14 @@ print("--" * 30)
 
 # Create training and test data
 training_data = SpeckDataset(5, 10**6, 5)
-test_data = SpeckDataset(5, 10**5, 5)
+test_data_batched = SpeckDataset(5, 10**5, 5)
+test_data = SpeckDataset(5, 10**5)
 
 # Creating the DataLoaders
 batch_size = 500
 
 train_dataloader = DataLoader(training_data, batch_size, shuffle=True)
+test_batched_dataloader = DataLoader(test_data_batched, 500, shuffle=True)
 test_dataloader = DataLoader(test_data, 500, shuffle=True)
 
 print("Initializing neural network")
@@ -61,36 +63,47 @@ def train(dataloader, model, loss_fn, optimizer):
             loss, current = loss.item(), batch*len(X)
             print("Loss: {:>7f}   [{:d}/{:d}]".format(loss, current//batch_size, size//batch_size))
 
-def test(dataloader, model, loss_fn):
+def test(dataloader, dataloader_batched, model, loss_fn):
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
     model.eval()
     test_loss, correct = 0,0
+    # Non Batched validation
     with torch.no_grad():
         for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            
+            prediction = model(X.float())
+
+            test_loss += loss_fn(prediction.float().reshape((-1,)), y.float()).item()
+            correct += torch.eq(torch.ge(prediction, 0.5), y).sum().item()
+
+    # Batched accuracy
+    with torch.no_grad():
+        for X, y in dataloader_batched:
             X, y = X.to(device), y.to(device)
 
             predictions = torch.empty((y.shape[0],X.shape[-1]))
 
-            # Compute prediction error
+            # Compute prediction error batched
             for group in range(5):
                 predictions[:,group] = model(X[:,:,:,group].float())
-
             
             prediction_avg = predictions.mean(axis=1).to(device)
 
-            test_loss += loss_fn(prediction_avg.float().reshape((-1,)), y.float()).item()
-            correct += torch.eq(torch.ge(prediction_avg, 0.5), y).sum().item()
+            test_loss_batched += loss_fn(prediction_avg.float().reshape((-1,)), y.float()).item()
+            correct_batched += torch.eq(torch.ge(prediction_avg, 0.5), y).sum().item()
 
     test_loss /= num_batches
     correct /= size
     print("Test Error: \nAccuracy: {:>0.1f}%, Avg loss: {:>8f} \n".format(100*correct, test_loss))
+    print("Test Error Batched: \nAccuracy: {:>0.1f}%, Avg loss: {:>8f} \n".format(100*correct_batched, test_loss_batched))
 
 epochs = 200
 for t in range(epochs):
     print("Epoch {}: \n".format(t+1) + "--"*30)
     train(train_dataloader, model, loss_fn, optimizer)
-    test(test_dataloader, model, loss_fn)
+    test(test_dataloader, test_batched_dataloader, model, loss_fn)
 print("Done!")
 
     
